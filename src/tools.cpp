e673e9deb3bc8a8f24e468072c4962afb8102157
Ming Lin <flaaud159@gmail.com>
2018-04-14 18:54:01 +0900

    Add files via upload
    my code

---
+++ src/tools.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+  TODO:
+	/* Calculate the RMSE here.
+	*/
+	//from udacity 
+	//I think I need to change dimensions here to calculate the rmse
+	cout << "initiallize vector rmse " << endl;
+	VectorXd rmse(4);
+	rmse << 0, 0, 0, 0;
+	if (estimations.size() != ground_truth.size() && estimations.size() == 0) {
+		cout << "Invalid estimation or ground_truth data" << endl;
+		return rmse;
+	}
+	cout << "accumulate squared residuals" << endl;
+	//accumulate squared residuals
+	for (int i = 0; i < estimations.size(); ++i) {
+		// ... your code here
+		VectorXd r;
+		r = estimations[i] - ground_truth[i];
+		r = r.array()*r.array();
+		rmse = rmse + r;
+	}
+	cout << "calculate the mean " << endl;
+	//calculate the mean
+	// ... your code here
+	rmse = rmse / estimations.size();
+
+	//calculate the squared root
+	// ... your code here
+	cout << "squared root calculation " << endl;
+	rmse = rmse.array().sqrt();
+	//return the result
+	return rmse;
+  */
+}
