a73219733d1cd3edd75ec39c64396a155676f12f
Ming Lin <flaaud159@gmail.com>
2018-04-15 18:59:14 +0900

    Update ukf.cpp

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -110,46 +110,55 @@ UKF::~UKF() {}
  * either radar or laser.
  */
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-  /**
-  TODO:
+ /**
+	TODO:
 
-  Complete this function! Make sure you switch between lidar and radar
-  measurements.
-  */
-  if ( !is_initialized_) {
-    if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
-      double rho = meas_package.raw_measurements_[0]; // range
-      double phi = meas_package.raw_measurements_[1]; // bearing
-      double rho_dot = meas_package.raw_measurements_[2]; // velocity of rh
-      double x = rho * cos(phi);
-      double y = rho * sin(phi);
-      double vx = rho_dot * cos(phi);
-  	  double vy = rho_dot * sin(phi);
-      double v = sqrt(vx * vx + vy * vy);
-      x_ << x, y, v, 0, 0;
-    } else {
-      x_ << meas_package.raw_measurements_[0], meas_package.raw_measurements_[1], 0, 0, 0;
-    }
+	Complete this function! Make sure you switch between lidar and radar
+	measurements.
+	*/
+	if (!is_initialized_) {
+		cout << "initialize lidar and radar" << endl;
+		if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+			double ro = meas_package.raw_measurements_(0);
+			double phi = meas_package.raw_measurements_(1);
+			double ro_dot = meas_package.raw_measurements_(2);
+			double px = ro * cos(phi);
+			double py = ro * sin(phi);
+			double vx = ro_dot * cos(phi);
+			double vy = ro_dot * sin(phi);
+			double v = sqrt(vx*vx + vy * vy);
+			x_ << px, py, v, 0, 0;
+		}
+		else {
+			x_ << meas_package.raw_measurements_(0), meas_package.raw_measurements_(1), 0, 0, 0;
+		}
 
-    // Saving first timestamp in seconds
-    time_us_ = meas_package.timestamp_ ;
-    // done initializing, no need to predict or update
-    is_initialized_ = true;
-    return;
-  }
+		// Saving first timestamp in seconds
+		time_us_ = meas_package.timestamp_;
+		// done initializing, no need to predict or update
+		is_initialized_ = true;
+		return;
+	}
 
-  // Calculate dt
-  double dt = (meas_package.timestamp_ - time_us_) / 1000000.0;
-  time_us_ = meas_package.timestamp_;
-  // Prediction step
-  Prediction(dt);
+	// Calculate dt
+	cout << "delta t calculation " << endl;
+	double dt = (meas_package.timestamp_ - time_us_) / 1000000.0;
+	time_us_ = meas_package.timestamp_;
+	// Prediction step
+	cout << "delta time is " << endl;
+	cout << dt << endl;
 
-  if (meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_) {
-    UpdateRadar(meas_package);
-  }
-  if (meas_package.sensor_type_ == MeasurementPackage::LASER && use_laser_) {
-    UpdateLidar(meas_package);
-  }
+	Prediction(dt);
+
+	if (meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_) {
+		cout << "Update Radar in process measurement step " << endl;
+		UpdateRadar(meas_package);
+	}
+	if (meas_package.sensor_type_ == MeasurementPackage::LASER && use_laser_) {
+		cout << "Update Lidar in process measurement step" << endl;
+		UpdateLidar(meas_package);
+	}
+
 }
 
 /**
