131de970886362c23a8c4ff50f0c2bbcaefe9b40
Ming Lin <flaaud159@gmail.com>
2018-04-15 17:00:36 +0900

    Update ukf.cpp

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -229,7 +229,7 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
   // 1. Predit measurement
   int n_z_ = 2;
   //dari MatrixXd Zsig = Xsig_pred_.block(0, 0, n_z, n_sig_);
-  MatrixXd Zsig_ = MatrixXd(n_z_, n_sig_ + 1);
+  MatrixXd Zsig_ = MatrixXd(n_z_, n_sig_);
 
   //mean predicted measurement
   VectorXd z_pred_ = VectorXd(n_z_);
